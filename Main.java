b17bb53e8cee410ded821339b5a798da54f9e562
Manikya Reddy <manikyareddy126@gmail.com>
2026-01-12 23:48:51 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,11 +4,16 @@ public class Main
 {
     public static void main(String[] args) 
     {
+        // Create an instance of StudentService to manage students
         StudentService service = new StudentService();
+        
+        // Create a Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
 
+        // Infinite loop to display the menu and perform operations until the user exits
         while (true) 
         {
+            // Displaying the menu options
             System.out.println("\n--- Student Management System ---");
             System.out.println("1. Add Student");
             System.out.println("2. Display Students");
@@ -18,11 +23,14 @@ public class Main
             System.out.println("6. Exit");
             System.out.print("Enter choice: ");
 
+            // Reading the user's menu choice
             int choice = sc.nextInt();
 
+            // Handling the user's choice with a switch statement
             switch (choice) 
             {
                 case 1:
+                    // Adding a new student
                     System.out.print("Enter ID: ");
                     int id = sc.nextInt();
                     sc.nextLine();
@@ -36,24 +44,29 @@ public class Main
                     System.out.print("Enter CGPA: ");
                     double cgpa = sc.nextDouble();
 
+                    // Create a new student object and add it using the service
                     service.addStudent(new Student(id, name, branch, cgpa));
                     break;
 
                 case 2:
+                    // Displaying all students
                     service.displayStudents();
                     break;
 
                 case 3:
+                    // Searching for a student by ID
                     System.out.print("Enter ID to search: ");
                     service.searchStudent(sc.nextInt());
                     break;
 
                 case 4:
+                    // Deleting a student by ID
                     System.out.print("Enter ID to delete: ");
                     service.deleteStudent(sc.nextInt());
                     break;
 
                 case 5:
+                    // Updating a student's details by ID
                     System.out.print("Enter ID to update: ");
                     int updateId = sc.nextInt();
                     sc.nextLine();
@@ -67,18 +80,22 @@ public class Main
                     System.out.print("Enter new CGPA: ");
                     double newCgpa = sc.nextDouble();
 
+                    // Update the student details using the service
                     service.updateStudent(updateId, newName, newBranch, newCgpa);
                     break;
                 
                 case 6:
+                    // Exiting the application
                     System.out.println("Exiting...");
                     sc.close();
                     return;
                     
                 default:
+                    // Handling invalid menu choices
                     System.out.println("Invalid choice.");
             }
         }
     }
 }
+
 
