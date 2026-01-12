85e860b7b7595262a8629b9f33ac0c28d19e9874
Manikya Reddy <manikyareddy126@gmail.com>
2026-01-12 23:44:04 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,10 +1,12 @@
 public class Student 
 {
+    // Private fields to store student information
     private int studentId;
     private String name;
     private String branch;
     private double cgpa;
 
+    // Constructor to initialize the student object
     public Student(int studentId, String name, String branch, double cgpa) 
     {
         this.studentId = studentId;
@@ -13,6 +15,7 @@ public class Student
         this.cgpa = cgpa;
     }
 
+    // Getter methods to access student information
     public int getStudentId() 
     {
         return studentId;
@@ -33,12 +36,14 @@ public class Student
         return cgpa;
     }
 
+    // Overriding the toString method to display student details
     @Override
     public String toString() 
     {
         return studentId + " | " + name + " | " + branch + " | " + cgpa;
     }
 
+    // Setter methods to update student information
     public void setName(String name) 
     {
     this.name = name;
@@ -55,3 +60,4 @@ public class Student
     }
 
 }
+
